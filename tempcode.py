2da95a04156ac4b2587ee6058b3af258ebb49c16
abhilashreddyy <abhilashreddyy1@gmail.com>
2017-08-26 09:46:10 +0530

    Update tempcode.py

--- tempcode.py
+++ tempcode.py
@@ -1,1 +1,20 @@
 
+>>> def simple_generator_function():
+>>>    yield 1
+>>>    yield 2
+>>>    yield 3
+
+>>> our_generator = simple_generator_function()
+>>> next(our_generator)
+1
+>>> next(our_generator)
+2
+>>> next(our_generator)
+3
+
+
+def get_primes(number):
+    while True:
+        if is_prime(number):
+            yield number
+        number += 1 # <<<<<<<<<<
